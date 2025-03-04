c4d371b66c28e60b405b85d8a5b618731de3f461
Sarah Pantzer <sarah@pantzer.me>
2025-03-04 11:34:53 +0100

    CSS hinzugefügt

--- app.py
+++ app.py
@@ -6,23 +6,24 @@ app = Flask(__name__)
 
 def categorize_bmi(bmi):
     if bmi < 18.5:
-        return "Du bist untergewichtig - iss  mehr"
+        return "Du bist untergewichtig - iss  mehr", "too-thin"
     elif bmi < 25:
-        return "Du hast ein normales Gewicht - mach weiter so"
+        return "Du hast ein normales Gewicht - mach weiter so", "normal"
     else:
-        return "Du bist übergewichtig - iss weniger und bewege dich mehr"
+        return "Du bist übergewichtig - iss weniger und bewege dich mehr", "too-fat"
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     bmi = None
     cat = None
+    classy = None
     if request.method == 'POST':
         height = float(request.form["height"])
         weight = float(request.form["weight"])
         bmi = round(weight / (height/100)**2, 2)
-        cat = categorize_bmi(bmi)
+        cat, classy = categorize_bmi(bmi)
 
-    return render_template('index.html', bmi=bmi, category=cat)
+    return render_template('index.html', bmi=bmi, category=cat, classy=classy)
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
