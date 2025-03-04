f64bbba03d329d2907d5474d522d4aa26c3cd8b8
Sarah Pantzer <sarah@pantzer.me>
2025-03-04 11:19:49 +0100

    main app functionality implemented

--- app.py
+++ app.py
@@ -4,9 +4,25 @@ app = Flask(__name__)
 
 # Definiere den Pfad für die Homepage
 
+def categorize_bmi(bmi):
+    if bmi < 18.5:
+        return "Du bist untergewichtig - iss  mehr"
+    elif bmi < 25:
+        return "Du hast ein normales Gewicht - mach weiter so"
+    else:
+        return "Du bist übergewichtig - iss weniger und bewege dich mehr"
+
 @app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    bmi = None
+    cat = None
+    if request.method == 'POST':
+        height = float(request.form["height"])
+        weight = float(request.form["weight"])
+        bmi = round(weight / (height/100)**2, 2)
+        cat = categorize_bmi(bmi)
+
+    return render_template('index.html', bmi=bmi, category=cat)
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
