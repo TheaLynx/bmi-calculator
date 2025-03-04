4664da5a44f384281067bf39b77d45bd340c9954
Sarah Pantzer <sarah@pantzer.me>
2025-03-04 10:46:38 +0100

    App developed

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+# Definiere den Pfad für die Homepage
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=False)
